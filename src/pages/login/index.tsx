5a5ff210192b0d8eee737895ba1b2686c61541b6
hingfei <soonghingfei@hotmail.com>
2023-04-12 01:03:27 +0800

    feat: login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,5 @@
 // ** React Imports
-import { ChangeEvent, MouseEvent, ReactNode, useState } from 'react'
+import { ReactNode, useEffect, useState } from 'react'
 
 // ** Next Imports
 import Link from 'next/link'
@@ -8,27 +8,25 @@ import { useRouter } from 'next/router'
 // ** MUI Components
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
-import TextField from '@mui/material/TextField'
-import InputLabel from '@mui/material/InputLabel'
 import Typography from '@mui/material/Typography'
-import IconButton from '@mui/material/IconButton'
 import CardContent from '@mui/material/CardContent'
-import FormControl from '@mui/material/FormControl'
-import OutlinedInput from '@mui/material/OutlinedInput'
-import { styled } from '@mui/material/styles'
+import { styled, Theme, useTheme } from '@mui/material/styles'
 import MuiCard, { CardProps } from '@mui/material/Card'
-import InputAdornment from '@mui/material/InputAdornment'
-import MuiFormControlLabel, { FormControlLabelProps } from '@mui/material/FormControlLabel'
 
 // ** Icons Imports
-import EyeOutline from 'mdi-material-ui/EyeOutline'
-import EyeOffOutline from 'mdi-material-ui/EyeOffOutline'
-
 // ** Configs
 import themeConfig from 'src/configs/themeConfig'
 
 // ** Layout Import
 import BlankLayout from 'src/@core/layouts/BlankLayout'
+import { FormProvider, useForm, useFormContext } from 'react-hook-form'
+import { getFormInputValues } from '../../@core/utils/get-form-input-values'
+import { PasswordInput, TextInput } from '../../@core/components/custom-inputs'
+import { PasswordComponent } from '../../views/sign-up/UserForm'
+import { Grid } from '@mui/material'
+import { useAuth } from '../../@core/context/authContext'
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { authConfig } from "../../configs/auth";
 
 // ** Demo Imports
 
@@ -48,108 +46,123 @@ const LinkStyled = styled('a')(({ theme }) => ({
   color: theme.palette.primary.main
 }))
 
-const FormControlLabel = styled(MuiFormControlLabel)<FormControlLabelProps>(({ theme }) => ({
-  '& .MuiFormControlLabel-label': {
-    fontSize: '0.875rem',
-    color: theme.palette.text.secondary
-  }
-}))
+const PasswordComponent = () => {
+  const { control } = useFormContext()
+  const [showPassword, setShowPassword] = useState(false)
 
+  // @ts-ignore
+  return (
+    <>
+      <Grid item mb={4}>
+        <PasswordInput
+          inputProps={{ label: 'Password' }}
+          controllerProps={{
+            control,
+            name: 'password'
+          }}
+          showPassword={showPassword}
+          toggleShowPassword={() => setShowPassword(!showPassword)}
+          isRequired
+        />
+      </Grid>
+    </>
+  )
+}
 
 const LoginPage = () => {
-  // ** State
-  const [values, setValues] = useState<State>({
-    password: '',
-    showPassword: false
-  })
+  const { handleLogin } = useAuth()
+  const formMethods = useForm()
 
-  // ** Hook
+  const {
+    handleSubmit,
+    control,
+    setError,
+    formState: { isSubmitting }
+  } = formMethods
+
   const router = useRouter()
 
-  const handleChange = (prop: keyof State) => (event: ChangeEvent<HTMLInputElement>) => {
-    setValues({ ...values, [prop]: event.target.value })
-  }
+  useEffect(() => {
+    const sessionToken = window.localStorage.getItem(authConfig.storageTokenKeyName)
+    if (sessionToken) {
+      router.push('/projects')
+    }
+  }, [])
 
-  const handleClickShowPassword = () => {
-    setValues({ ...values, showPassword: !values.showPassword })
-  }
 
-  const handleMouseDownPassword = (event: MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault()
+  const onSubmit = async (values: any) => {
+    const input = getFormInputValues(values)
+    console.log('input', input)
+
+    await handleLogin(input.userName, input.password)
   }
 
   return (
-    <Box display={'flex'} alignItems={'center'} justifyContent={'center'} minHeight={'80vh'}>
-      <Card sx={{ zIndex: 1 }}>
-        <CardContent sx={{ padding: theme => `${theme.spacing(12, 9, 7)} !important` }}>
-          <Box sx={{ mb: 8, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-            <Typography
-              variant='h6'
-              sx={{
-                ml: 3,
-                lineHeight: 1,
-                fontWeight: 600,
-                textTransform: 'uppercase',
-                fontSize: '1.5rem !important'
-              }}
-            >
-              {themeConfig.templateName}
-            </Typography>
-          </Box>
-          <Box sx={{ mb: 6 }}>
-            <Typography variant='h5' sx={{ fontWeight: 600, marginBottom: 1.5 }}>
-              Welcome to {themeConfig.templateName}! 👋🏻
-            </Typography>
-            <Typography variant='body2'>Please sign-in to your account and start the journey</Typography>
-          </Box>
-          <form noValidate autoComplete='off' onSubmit={e => e.preventDefault()}>
-            <TextField autoFocus fullWidth id='email' label='Email' sx={{ marginBottom: 4 }}/>
-            <FormControl fullWidth>
-              <InputLabel htmlFor='auth-login-password'>Password</InputLabel>
-              <OutlinedInput
-                label='Password'
-                value={values.password}
-                id='auth-login-password'
-                onChange={handleChange('password')}
-                type={values.showPassword ? 'text' : 'password'}
-                endAdornment={
-                  <InputAdornment position='end'>
-                    <IconButton
-                      edge='end'
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                      aria-label='toggle password visibility'
-                    >
-                      {values.showPassword ? <EyeOutline/> : <EyeOffOutline/>}
-                    </IconButton>
-                  </InputAdornment>
-                }
-              />
-            </FormControl>
-            <Button
-              fullWidth
-              size='large'
-              variant='contained'
-              sx={{ marginBottom: 7, marginTop: 5 }}
-              onClick={() => router.push('/')}
-            >
-              Sign In
-            </Button>
-            <Box sx={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap', justifyContent: 'center' }}>
-              <Typography variant='body2' sx={{ marginRight: 2 }}>
-                New on our platform?
-              </Typography>
-              <Typography variant='body2'>
-                <Link passHref href='/sign-up'>
-                  <LinkStyled>Create an account</LinkStyled>
-                </Link>
+    <FormProvider {...formMethods}>
+      <Box display={'flex'} alignItems={'center'} justifyContent={'center'} minHeight={'80vh'}>
+        <Card sx={{ zIndex: 1 }}>
+          <CardContent sx={{ padding: theme => `${theme.spacing(12, 9, 7)} !important` }}>
+            <Box sx={{ mb: 8, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+              <Typography
+                variant='h6'
+                sx={{
+                  // ml: 3,
+                  lineHeight: 1,
+                  fontWeight: 600,
+                  textTransform: 'uppercase',
+                  fontSize: '1.5rem !important'
+                }}
+              >
+                {themeConfig.templateName}
               </Typography>
             </Box>
-          </form>
-        </CardContent>
-      </Card>
-      {/*<FooterIllustrationsV1/>*/}
-    </Box>
+            <Box sx={{ mb: 6 }}>
+              <Typography variant='h5' sx={{ fontWeight: 600, marginBottom: 1.5 }}>
+                Welcome to {themeConfig.templateName}! 👋🏻
+              </Typography>
+              <Typography variant='body2'>Please sign-in to your account and start the journey</Typography>
+            </Box>
+
+            <form noValidate autoComplete='off' onSubmit={handleSubmit(onSubmit)}>
+              <TextInput
+                inputProps={{
+                  label: 'Username',
+                  sx: { marginBottom: 4 }
+                }}
+                controllerProps={{
+                  control,
+                  name: 'userName'
+                }}
+                isRequired
+              />
+              <PasswordComponent />
+
+              <Button
+                fullWidth
+                size='large'
+                variant='contained'
+                sx={{ marginBottom: 7, marginTop: 5 }}
+                onClick={handleSubmit(onSubmit)}
+                disabled={isSubmitting}
+              >
+                Sign In
+              </Button>
+              <Box sx={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap', justifyContent: 'center' }}>
+                <Typography variant='body2' sx={{ marginRight: 2 }}>
+                  New on our platform?
+                </Typography>
+                <Typography variant='body2'>
+                  <Link passHref href='/sign-up'>
+                    <LinkStyled>Create an account</LinkStyled>
+                  </Link>
+                </Typography>
+              </Box>
+            </form>
+          </CardContent>
+        </Card>
+        {/*<FooterIllustrationsV1/>*/}
+      </Box>
+    </FormProvider>
   )
 }
 
