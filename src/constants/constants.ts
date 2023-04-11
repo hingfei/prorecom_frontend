6c498ad41bf3e1b5946baff49a5fca4fd2c5be0a
hingfei <soonghingfei@hotmail.com>
2023-04-12 00:59:08 +0800

    feat: constants

---
+++ src/constants/constants.ts
@@ -0,0 +1,77 @@
+export const DrawerType = {
+  editJobSeekerForm: 'editJobSeekerForm',
+  editEducationForm: 'editEducationForm',
+  editAboutForm: 'editAboutForm',
+  editPasswordForm: 'editPasswordForm'
+}
+
+export const companySize = [
+  { label: '0 - 10', value: 'extra_small' },
+  { label: '11 - 50', value: 'small' },
+  { label: '51 - 200', value: 'medium' },
+  { label: '201 - 500', value: 'large' },
+  { label: '501 - 1000', value: 'extra_large' }
+]
+
+export const genderSelect = [
+  { label: 'Male', value: 'male' },
+  { label: 'Female', value: 'female' }
+]
+
+export const eduLevelSelect = [
+  {
+    label: 'Secondary School',
+    value: 1
+  },
+  {
+    label: 'Pre-University (A-Levels, Foundation, Diploma or equivalent)',
+    value: 2
+  },
+  {
+    label: "Undergraduate (Bachelor's Degree or equivalent)",
+    value: 3
+  },
+  {
+    label: "Postgraduate (Master's Degree or equivalent or higher)",
+    value: 4
+  },
+  {
+    label: 'Other',
+    value: 5
+  }
+]
+
+export const fieldStudySelect = [
+  {
+    label: 'Business',
+    value: 1
+  },
+  {
+    label: 'Creative Arts, Design, Architecture',
+    value: 2
+  },
+  {
+    label: 'Engineering',
+    value: 3
+  },
+  {
+    label: 'Health Science',
+    value: 4
+  },
+  {
+    label: 'Hospitality, Tourism and Culinary',
+    value: 5
+  },
+  {
+    label: 'IT',
+    value: 6
+  },
+  {
+    label: 'Law',
+    value: 7
+  },
+  {
+    label: 'Social Science',
+    value: 8
+  }
+]
