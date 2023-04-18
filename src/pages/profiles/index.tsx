68197492160952d915635171611f3b8db5495559
hingfei <soonghingfei@hotmail.com>
2023-04-18 22:14:07 +0800

    feat: profile call me query refactor

--- src/pages/profiles/index.tsx
+++ src/pages/profiles/index.tsx
@@ -1,79 +1,76 @@
-import { Box, Button, Card, CardContent, Grid, Typography } from '@mui/material'
-import { useJobSeekerDetailQuery } from '../../graphql/api'
-import Chip from '@mui/material/Chip'
+import { Card, CardContent, Grid } from '@mui/material'
+import { useJobSeekerDetailLazyQuery, useMeQuery } from '../../graphql/api'
 import Spinner from '../../@core/components/spinner'
-import { useRouter } from "next/router";
-import { useState } from "react";
+import PageHeader from '../../@core/components/page-header'
+import Divider from '@mui/material/Divider'
+import EducationSection from '../../views/profiles/Education/EducationSection'
+import PersonalInfoSection from '../../views/profiles/PersonalInfo/PersonalInfoSection'
+import ResumeSection from '../../views/profiles/Resume/ResumeSection'
+import AboutSection from '../../views/profiles/About/AboutSection'
+import PasswordSection from '../../views/profiles/Password/PasswordSection'
+import { onError } from '../../@core/utils/response'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
 
 const Profile = () => {
-  // const [loading, setLoading] = useState(true);
-  // const router = useRouter();
-  // const { userId } = router.query;
-  //
-  // if (!userId) {
-  //   return <Spinner />
-  // } else {
-  //   setLoading(false)
-  // }
-  //
-  // const { data, loading: detailLoading } = useJobSeekerDetailQuery({
-  //   variables: {
-  //     seekerId: parseInt(userId)
-  //   },
-  //   fetchPolicy: 'no-cache'
-  // })
+  const [loading, setLoading] = useState(true)
+  const router = useRouter()
+
+  const { data: userData } = useMeQuery({
+    onCompleted: data => {
+      console.log('data,', data)
+      fetchJobSeeker({ variables: { seekerId: parseInt(data?.me?.userId) } })
+    },
+    onError: error => {
+      console.log(error)
+      router.push('/401')
+      onError(error, undefined)
+    }
+  })
+
+  const [fetchJobSeeker, { data }] = useJobSeekerDetailLazyQuery({
+    onCompleted: data => {
+      setLoading(false)
+    },
+    onError: error => {
+      router.push('/404')
+      onError(error, undefined)
+    }
+  })
+
+  if (loading) {
+    return <Spinner />
+  }
 
   return (
     <Grid container spacing={6}>
-      {/*<Grid item xs={4.5}>*/}
-      {/*  <Card>*/}
-      {/*    <CardContent sx={{ paddingX: 6, paddingY: 5 }}>*/}
-      {/*      <Typography variant={'h6'} fontWeight={700}>*/}
-      {/*        {item.projectName}*/}
-      {/*      </Typography>*/}
-      {/*      <Box display={'flex'} justifyContent={'space-between'} alignItems={'center'} pb={5}>*/}
-      {/*        <Typography variant={'body2'}>{item.company?.companyName}</Typography>*/}
-      {/*        <Typography variant={'body2'}>{item.company?.companyState}</Typography>*/}
-      {/*      </Box>*/}
-      {/*      <Box display={'flex'} justifyContent={'space-between'} alignItems={'center'}>*/}
-      {/*        <Chip*/}
-      {/*          size='medium'*/}
-      {/*          variant='outlined'*/}
-      {/*          label={item.projectTypes}*/}
-      {/*          color='primary'*/}
-      {/*          sx={{ fontSize: '13px', fontWeight: 500, borderRadius: '18px' }}*/}
-      {/*        />*/}
-      {/*        <Typography variant={'body2'}>{item.projectSalary}</Typography>*/}
-      {/*      </Box>*/}
-      {/*    </CardContent>*/}
-      {/*  </Card>*/}
-      {/*</Grid>*/}
-      {/*<Grid item xs={7.5}>*/}
-      {/*  <Card>*/}
-      {/*    <CardContent>*/}
-      {/*      <Box mb={14}>*/}
-      {/*        <Box display={'flex'} justifyContent={'space-between'} alignItems={'center'} pb={3}>*/}
-      {/*          <Typography variant={'h5'} fontWeight={700}>*/}
-      {/*            {project?.projectName ?? '-'}*/}
-      {/*          </Typography>*/}
-      {/*          <Button variant={'contained'}>Apply</Button>*/}
-      {/*        </Box>*/}
-      {/*        <Box display={'flex'} justifyContent={'space-between'} alignItems={'center'} pb={1}>*/}
-      {/*          <Typography variant={'body1'}>{project?.company?.companyName ?? '-'}</Typography>*/}
-      {/*          <Typography variant={'body1'}>{project?.company?.companyFounder}</Typography>*/}
-      {/*        </Box>*/}
-      {/*        <Typography variant={'body2'} pb={2}>*/}
-      {/*          {project?.company?.companySize ? `${project?.company?.companySize} employer` : ''}*/}
-      {/*        </Typography>*/}
-      {/*        <Box pb={5}>*/}
-      {/*          <Typography variant={'body2'}>{project?.company?.companyStreet}</Typography>*/}
-      {/*          <Typography variant={'body2'}>{project?.company?.companyCity}</Typography>*/}
-      {/*          <Typography variant={'body2'}>{project?.company?.companyState}</Typography>*/}
-      {/*        </Box>*/}
-      {/*      </Box>*/}
-      {/*    </CardContent>*/}
-      {/*  </Card>*/}
-      {/*</Grid>*/}
+      <PageHeader title={'Profile'} />
+      <Grid item xs={12} md={5} lg={4}>
+        <Card>
+          <CardContent sx={{ py: 10, display: 'flex', alignItems: 'center', flexDirection: 'column', rowGap: '6px' }}>
+            <PersonalInfoSection seekerId={data?.jobSeekerDetail?.seekerId} jobSeeker={data?.jobSeekerDetail} />
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid item xs={12} md={7} lg={8}>
+        <Card>
+          <CardContent sx={{ py: 10, px: '32px !important' }}>
+            <AboutSection aboutData={data?.jobSeekerDetail?.seekerAbout} seekerId={data?.jobSeekerDetail?.seekerId} />
+            <Divider sx={{ mb: 5 }} />
+
+            <EducationSection
+              edu={data?.jobSeekerDetail?.seekerHighestEduc}
+              seekerId={data?.jobSeekerDetail?.seekerId}
+            />
+            <Divider sx={{ mb: 5 }} />
+
+            <ResumeSection jobSeeker={data?.jobSeekerDetail} seekerId={data?.jobSeekerDetail?.seekerId} />
+            <Divider sx={{ mb: 5 }} />
+
+            <PasswordSection seekerId={data?.jobSeekerDetail?.seekerId} />
+          </CardContent>
+        </Card>
+      </Grid>
     </Grid>
   )
 }
