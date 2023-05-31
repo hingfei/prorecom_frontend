9f2d9688b5d5af5998fd5ec0d1cbf33af0ff6eaa
hingfei <soonghingfei@hotmail.com>
2023-05-31 13:50:26 +0800

    feat: update withAuth HOC to recognize user type

--- src/@core/hooks/withAuth.tsx
+++ src/@core/hooks/withAuth.tsx
@@ -4,7 +4,7 @@ import { useAuth } from '../context/authContext'
 import toast from 'react-hot-toast'
 import Spinner from '../components/spinner'
 
-const withAuth = <P extends object>(Component: React.ComponentType<P>) => {
+const withAuth = <P extends object>(Component: React.ComponentType<P>, allowedUserTypes: string[]) => {
   const Auth = (props: P) => {
     const [isLoading, setIsLoading] = useState(true)
     const { isAuthenticated, isInitialized, fetchMe } = useAuth()
@@ -14,11 +14,29 @@ const withAuth = <P extends object>(Component: React.ComponentType<P>) => {
       const checkAuth = async () => {
         if (isInitialized) {
           await fetchMe()
-          setIsLoading(false)
+          const data = window.localStorage.getItem('userData')
+          let userData
+          if (data) {
+            userData = JSON.parse(data)
+          }
+          if (data && !allowedUserTypes.includes(userData.userType)) {
+            router.push('/403')
+            toast.error('Unauthorized access.')
+          } else {
+            setIsLoading(false)
+          }
         } else {
+          const data = window.localStorage.getItem('userData')
+          let userData
+          if (data) {
+            userData = JSON.parse(data)
+          }
           if (!isAuthenticated) {
             router.push('/401')
             toast.error('Please login before proceeding.')
+          } else if (data && !allowedUserTypes.includes(userData.userType)) {
+            router.push('/403')
+            toast.error('Unauthorized access.')
           } else {
             setIsLoading(false)
           }
