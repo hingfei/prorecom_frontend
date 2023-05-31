d0cbcb28fffdbb64ff91d22500bd6a013fddcb72
hingfei <soonghingfei@hotmail.com>
2023-05-31 13:52:26 +0800

    feat: icon button components

---
+++ src/@core/components/icons/index.tsx
@@ -0,0 +1,31 @@
+import { IconButton, Tooltip } from '@mui/material'
+import { DeleteOutline, PencilOutline } from 'mdi-material-ui'
+import { IconButtonProps } from '@mui/material';
+
+export type IconBaseProps = IconButtonProps & { title?: string; children: React.ReactNode };
+
+const IconBase = ({ title, children, ...props }: IconBaseProps) => {
+  return title ? (
+    <Tooltip title={title}>
+      <IconButton {...props}>{children}</IconButton>
+    </Tooltip>
+  ) : (
+    <IconButton {...props}>{children}</IconButton>
+  )
+}
+
+export const EditIcon = (props: Omit<IconBaseProps, 'children'>) => {
+  return (
+    <IconBase {...props}>
+      <PencilOutline fontSize='small' />
+    </IconBase>
+  )
+}
+
+export const DeleteIcon = (props: Omit<IconBaseProps, 'children'>) => {
+  return (
+    <IconBase {...props}>
+      <DeleteOutline fontSize='small' />
+    </IconBase>
+  )
+}
